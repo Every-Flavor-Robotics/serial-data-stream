e279815c7cb66d57cefe543a85255bc8cb749203
Swapnil Pande <swapnil.pande98@gmail.com>
2023-05-16 16:43:16 -0400

    Set up platformio project

--- include/serial_data_stream.h
+++ include/serial_data_stream.h
@@ -8,107 +8,166 @@
 
 void close(const char* fname)
 {
-    Serial.print("_data_stream_:_close_:");
-    Serial.println(fname);
+  Serial.print("_data_stream_:_close_:");
+  Serial.println(fname);
 }
 
 // Templatized version of data stream
 template <typename T>
 class DataStream
 {
-public:
-    DataStream(const char* name, int num_data_points);
+ public:
+  DataStream(const char* name, int num_data_points);
+  DataStream(const DataStream<T>& other);
+  ~DataStream();
+  DataStream<T>& operator=(const DataStream<T>& other);
 
-    const char* get_name();
-    int get_num_data_points();
-    T* get_data();
+  const char* get_name();
+  int get_num_data_points();
+  int get_size();
+  const T* get_data();
 
-    // Add a new data point to the data stream
-    // This will append until the data stream is full, then it will return false and not add the data point
-    bool add_data_point(T data_point);
+  // Add a new data point to the data stream
+  // This will append until the data stream is full, then it will return false
+  // and not add the data point
+  bool add_data_point(T data_point);
 
-    // Delete the existing data
-    void reset();
+  // Delete the existing data
+  void reset();
 
-    // Flush data stream to serial
-    // Allow user to specify a postfix to append to the name of the data stream
-    void output_data_stream(const char* postfix);
-private:
+  // Flush data stream to serial
+  // Allow user to specify a postfix to append to the name of the data stream
+  void output_data_stream(const char* postfix);
 
-    const char* name;
-    int num_data_points;
-    T* data;
-    int cur_data_point = 0;
+ private:
+  const char* name;
+  int num_data_points;
+  T* data;
+  int cur_data_point = 0;
 };
 
 template <typename T>
 DataStream<T>::DataStream(const char* name, int num_data_points)
 {
-    this->name = name;
-    this->num_data_points = num_data_points;
-    this->data = new T[num_data_points];
+  this->name = name;
+  this->num_data_points = num_data_points;
+  this->data = new T[num_data_points];
 }
 
+template <typename T>
+DataStream<T>::~DataStream()
+{
+  delete[] data;
+  data = nullptr;
+}
+
+// Assignment operator
+template <typename T>
+DataStream<T>& DataStream<T>::operator=(const DataStream<T>& other)
+{
+  if (this != &other)
+  {
+    this->name = other.name;
+    this->num_data_points = other.num_data_points;
+    this->data = new T[num_data_points];
+
+    for (int i = 0; i < num_data_points; i++)
+    {
+      this->data[i] = other.data[i];
+    }
+  }
+
+  return *this;
+}
+
+// Copy constructor
+template <typename T>
+DataStream<T>::DataStream(const DataStream<T>& other)
+{
+  this->name = other.name;
+  this->num_data_points = other.num_data_points;
+  this->data = new T[num_data_points];
+
+  for (int i = 0; i < num_data_points; i++)
+  {
+    this->data[i] = other.data[i];
+  }
+}
+
+// Return name of the data stream
 template <typename T>
 const char* DataStream<T>::get_name()
 {
-    return name;
+  return name;
 }
 
+// Return size of the data stream
+template <typename T>
+int DataStream<T>::get_size()
+{
+  return num_data_points;
+}
+
+// Return number of data points in the data stream
 template <typename T>
 int DataStream<T>::get_num_data_points()
 {
-    return num_data_points;
+  return num_data_points;
 }
 
+// Return pointer to the data
+// This could be dangerous (dangling pointer) if the data stream is deleted
+// However, this is preferred over returning a copy, which could be expensive
 template <typename T>
-T* DataStream<T>::get_data()
+const T* DataStream<T>::get_data()
 {
-    return data;
+  return data;
 }
 
+// Add a new data point to the data stream
+// This could
 template <typename T>
 bool DataStream<T>::add_data_point(T data_point)
 {
-    if(cur_data_point < num_data_points)
-    {
-        // Append data and increment counter
-        data[cur_data_point] = data_point;
-        cur_data_point++;
+  if (cur_data_point < num_data_points)
+  {
+    // Append data and increment counter
+    data[cur_data_point] = data_point;
+    cur_data_point++;
 
-        return true;
-    }
+    return true;
+  }
 
-    return false;
+  return false;
 }
 
 template <typename T>
 void DataStream<T>::reset()
 {
-    cur_data_point = 0;
+  // Don't need to do anything else, just reset the counter
+  cur_data_point = 0;
 }
-
 
 // Templatized function to output a data stream to a serial port
 template <typename T>
 void DataStream<T>::output_data_stream(const char* postfix)
 {
-    // Print the name of the data stream
-    Serial.print("_data_stream_:");
-    Serial.print(get_name());
-    Serial.print("_");
-    Serial.print(postfix);
-    Serial.print(":");
+  // Print the name of the data stream
+  Serial.print("_data_stream_:");
+  Serial.print(get_name());
+  Serial.print("_");
+  Serial.print(postfix);
+  Serial.print(":");
 
-    // Print the data
-    for(int i = 0; i < get_num_data_points(); i++)
-    {
-        Serial.print(data[i], 10);
-        Serial.print(",");
-    }
+  // Print the data
+  for (int i = 0; i < get_num_data_points(); i++)
+  {
+    Serial.print(data[i], 10);
+    Serial.print(",");
+  }
 
-    // Print a newline
-    Serial.println();
+  // Print a newline
+  Serial.println();
 }
 
 #endif
