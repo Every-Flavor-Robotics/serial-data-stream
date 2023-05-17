0eb0f2d58648748a1e0ff487d82735aaf67658c4
Swapnil Pande <swapnil.pande98@gmail.com>
2023-05-17 14:58:53 -0400

    Added docstrings to functions

--- include/serial_data_stream.h
+++ include/serial_data_stream.h
@@ -9,37 +9,85 @@
 namespace DataStream
 {
 
+/**
+ * Indicates the termination of data logging to the serial reading script.
+ * @param fname The name of the file to write data to.
+ */
 void close(const char* fname)
 {
   Serial.print("_data_stream_:_close_:");
   Serial.println(fname);
 }
 
-// Templatized version of data stream
+// Templatized implementation of data stream
 template <typename T>
 class DataStream
 {
  public:
+  /**
+   * Constructs a new data stream object with the specified name and size.
+   * @param name The name of the data stream.
+   * @param num_data_points The number of data points the stream can hold.
+   */
   DataStream(const char* name, int num_data_points);
+
+  /**
+   * Copy constructor for a data stream object.
+   * @param other The data stream object to copy from.
+   */
   DataStream(const DataStream<T>& other);
+
+  /**
+   * Destructor for a data stream object.
+   */
   ~DataStream();
+
+  /**
+   * Assignment operator for a data stream object.
+   * @param other The data stream object to assign from.
+   * @return The updated data stream object.
+   */
   DataStream<T>& operator=(const DataStream<T>& other);
 
+  /**
+   * Returns the name of the data stream.
+   * @return The name of the data stream.
+   */
   const char* get_name();
+
+  /**
+   * Returns the number of data points that have been added to the data stream.
+   * @return The number of data points in the data stream.
+   */
   int get_num_data_points();
+
+  /**
+   * Returns the number of data points the stream can hold.
+   * @return The number of data points the stream can hold.
+   */
   int get_size();
   const T* get_data();
 
-  // Add a new data point to the data stream
-  // This will append until the data stream is full, then it will return false
-  // and not add the data point
+  /**
+   * Adds a new data point to the data stream. Will not add data if the stream
+   * is full.
+   * @param data_point The data point to add.
+   * @return True if the data point was added successfully, false if the stream
+   * is full.
+   */
   bool add_data_point(T data_point);
 
-  // Delete the existing data
+  /**
+   * Resets the data stream, deleting all existing data. Implementation simply
+   * changes the counter, does not actually delete the data.
+   */
   void reset();
 
-  // Flush data stream to serial
-  // Allow user to specify a postfix to append to the name of the data stream
+  /**
+   * Outputs the data stream to a serial port. The data will be the sent with
+   * the name "get_name() + postfix".
+   * @param postfix The postfix to append to the stream name.
+   */
   void output_data_stream(const char* postfix);
 
  private:
